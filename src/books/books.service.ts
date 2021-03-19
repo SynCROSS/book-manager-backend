2ab46a55a202087ba39cece04ee55c7d09acf3e4
SynCROSS <kuuhaku3125@gmail.com>
2021-03-19 15:01:00 +0900

    Update books.service.ts

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { config } from 'dotenv';
 import { Repository } from 'typeorm';
 import { Book } from '../entity/book.entity';
 import { BookDTO } from '../dto/book.dto';
-
-config();
-
 @Injectable()
 export class BooksService {
   constructor(
