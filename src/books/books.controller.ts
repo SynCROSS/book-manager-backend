e0d5c335c59284b9913b113901fe568f8a3a5ff9
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-03-22 18:50:57 +0900

    That's it for today

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,19 +1,29 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BookDTO } from '../dto/book.dto';
 import { Book } from 'src/entity/book.entity';
 
 @Controller('books')
 export class BooksController {
-  constructor(private readonly bookService: BooksService) {}
+  constructor(private readonly booksService: BooksService) {}
+
+  @Get()
+  getBooks(@Query('limit') limit: number) {
+    return this.booksService.getBooks(limit);
+  }
+
+  @Get('search')
+  searchBookByTitle(@Query('title') title: string) {
+    return this.booksService.searchBookByTitle(title);
+  }
+
+  @Get(':id')
+  getBookById(@Param('id') id: number) {
+    return this.booksService.getBookById(id);
+  }
 
   @Post()
   addBook(@Body() bookDTO: BookDTO): Promise<Book> {
-    return this.bookService.addBook(bookDTO);
-  }
-
-  @Get()
-  getAllBooks() {
-    return this.bookService.getAllBooks();
+    return this.booksService.addBook(bookDTO);
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { Book } from '../entity/book.entity';
 import { BookDTO } from '../dto/book.dto';
 @Injectable()
@@ -8,26 +8,35 @@ export class BooksService {
   constructor(
     @InjectRepository(Book) private readonly bookRepository: Repository<Book>,
   ) {}
-  // private BASE_URL =
-  //   'http://www.librarything.com/api_getdata.php?userid=timspalding&responseType=json';
 
-  addBook(bookDTO: BookDTO): Promise<Book> {
+  async getBooks(numberOfBooks: number): Promise<Book[]> {
+    const books = await this.bookRepository.find({
+      take: numberOfBooks,
+    });
+    return books;
+  }
+
+  async searchBookByTitle(title: string): Promise<Book[]> {
+    const books = await this.bookRepository.find({
+      title: Like('%' + title + '%'),
+    });
+
+    return books;
+  }
+
+  async getBookById(id: number): Promise<Book> {
+    const book = await this.bookRepository.findOne({
+      book_id: id,
+    });
+
+    return book;
+  }
+
+  async addBook(bookDTO: BookDTO) {
     const book = this.bookRepository.create({
       ...bookDTO,
     });
 
-    return this.bookRepository.save(book);
-  }
-
-  async getAllBooks() {
-    try {
-      // const {
-      //   data: { books },
-      // } = await axios.get(this.BASE_URL);
-
-      return this.bookRepository.find();
-    } catch (e) {
-      console.error(e);
-    }
+    return await this.bookRepository.save(book);
   }
 }
