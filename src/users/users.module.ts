3ceb2374125b4ba0e60dc326a1472c62ec10fcfa
SynCROSS <kuuhaku3125@gmail.com>
2021-03-23 09:50:52 +0900

    Add Password Table

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,12 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entity/user.entity';
+import { Password } from 'src/entity/password.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Password])],
   providers: [UsersService],
   controllers: [UsersController],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,18 +3,31 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entity/user.entity';
 import { Repository } from 'typeorm';
 import { UserDTO } from '../dto/user.dto';
-
+import { hashSync } from 'bcrypt';
+import { Password } from 'src/entity/password.entity';
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @InjectRepository(Password)
+    private readonly passwordRepository: Repository<Password>,
   ) {}
 
-  addUser(userDTO: UserDTO): Promise<User> {
+  async addUser(userDTO: UserDTO): Promise<User> {
+    const hash = hashSync(userDTO.password, 10);
+
     const user = this.userRepository.create({
       ...userDTO,
+      password: hash,
     });
 
-    return this.userRepository.save(user);
+    const hashedPassword = this.passwordRepository.create({
+      hash,
+      owner: user,
+    });
+
+    await this.passwordRepository.save(hashedPassword);
+
+    return await this.userRepository.save(user);
   }
 }
